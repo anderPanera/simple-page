e58960fc1f60489ea7caf64a8a408af8529bc4d8
anderPanera <anderpanera@gmail.com>
2024-01-17 22:57:47 +0100

    main page created

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,30 +1,136 @@
+import { motion, AnimatePresence } from "framer-motion"
+import { useRef, useState } from "react"
+import { useEffect } from "react"
+
 export default function MainPage() {
+
+    const divRef = useRef()
+
+    const [currentY, setCurrentY] = useState(0)
+    const [direction, setDirection] = useState(0)
+    const [anim, setAnim] = useState(false)
+
+    const [items, setItems] = useState([
+        1, 2, 3, 4, 5, 6, 7, 8
+    ])
+
+
+    useEffect(() => {
+        divRef.current.addEventListener('wheel', handleWheel)
+        return () => {
+            divRef.current.removeEventListener('wheel', handleWheel)
+        }
+    }, [anim])
+
+    const handleWheel = (e) => {
+        e.preventDefault()
+        if (currentY == 0 && e.deltaY == -100) return
+        if (currentY == 7 && e.deltaY == 100) return
+        if (!anim) {
+            setDirection(e.deltaY)
+            setAnim(_ => true)
+            setTimeout(() => {
+                setAnim(_ => false)
+            }, 1000);
+            setCurrentY(prev => Math.min(Math.max(prev + e.deltaY / 100, 0), 7))
+        }
+    }
+
+    const handleClick = (e, id) => {
+        e.preventDefault()
+        if (currentY == id) return
+        if (currentY < id) setDirection(100)
+        else setDirection(-100)
+        setAnim(_ => true)
+        setTimeout(() => {
+            setAnim(_ => false)
+        }, 600);
+        setCurrentY(id)
+    }
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+    }
+
     return (
-        <div className="overflow-hidden">
-            <div className="w-screen h-screen bg-1">
+        <div className="h-screen w-screen flex bg-bg p-5" ref={divRef}>
+            <div className="h-full w-36 shrink-0 flex flex-col ">
+                {items.map((val, i) => (
+                    <div
+                        key={i}
+                        className={classNames('flex-1 w-full cursor-pointer', 'bg-' + (i + 1))}
+                        onClick={(e) => handleClick(e, i)}
+                    >
 
+                    </div>
+                ))}
             </div>
-            <div className="w-screen h-screen bg-2">
+            <div className={classNames('w-full h-full ease-in-out duration-[2000ms]', 'bg-' + (currentY + 1))}>
+                <AnimatePresence>
+                    {items.map((val, i) => i == currentY && !anim &&
+                        <motion.div
+                            key={i}
+                            initial={{ opacity: 0, }}
+                            animate={{ opacity: 1, }}
+                            exit={{ opacity: 0 }}
+                            transition={{
+                                duration: 0.5,
+                            }}
+                            className="h-full text-white absolute font-akira p-10 text-8xl flex flex-col justify-evenly"
+                        >
+                            {!anim && <motion.span className="h-full"
+                                initial={{ opacity: 0, y: direction }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -direction }}
+                                transition={{
+                                    duration: 0.3,
+                                    delay: 0.1
+                                }}
+                            >
+                                {val}.
+                            </motion.span>}
 
-            </div>
-            <div className="w-screen h-screen bg-3">
 
-            </div>
-            <div className="w-screen h-screen bg-4">
+                            <motion.div className="h-1/2"
+                                initial={{ opacity: 0, y: direction }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -direction }}
+                                transition={{
+                                    duration: 0.3,
+                                    delay: 0.2
+                                }}
+                            >
+                                <img className="h-full" src="https://www.onlygfx.com/wp-content/uploads/2022/04/angry-eye-tribal-tattoo-1325.svg"></img>
+                            </motion.div>
+                            {!anim && <motion.span className="h-full"
+                                initial={{ opacity: 0, y: direction }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -direction }}
+                                transition={{
+                                    duration: 0.3,
+                                    delay: 0.3
+                                }}
+                            >
+                                TATTOO.
+                            </motion.span>}
 
-            </div>
-            <div className="w-screen h-screen bg-5">
 
+                            {!anim && <motion.span className="h-full"
+                                initial={{ opacity: 0, y: direction }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -direction }}
+                                transition={{
+                                    duration: 0.3,
+                                    delay: 0.4
+                                }}
+                            >
+                                TATTOOTATTOO.
+                            </motion.span>}
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
-            <div className="w-screen h-screen bg-6">
-
-            </div>
-            <div className="w-screen h-screen bg-7">
-
-            </div>
-            <div className="w-screen h-screen bg-8">
-
-            </div>
+            <span className="bg-1 bg-2 bg-3 bg-4 bg-5 bg-6 bg-7 bg-8"></span>
         </div>
     )
 }
